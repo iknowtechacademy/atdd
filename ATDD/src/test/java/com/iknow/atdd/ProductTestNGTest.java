539f6d366d05366c4bbc14e4f60f39d63de3ab34
gustavo-palabral-tierconnect-com <gustavo.palabral@tierconnect.com>
2020-08-05 20:15:50 -0400

    Agregando el test case de Creacion de Producto

--- ATDD/src/test/java/com/iknow/atdd/ProductTestNGTest.java
+++ ATDD/src/test/java/com/iknow/atdd/ProductTestNGTest.java
@@ -33,19 +33,74 @@ public class ProductTestNGTest {
         String baseUrl = "http://localhost:9090/JsfDemoApp/faces/welcomeJSF.jsp";
         
         driver.get(baseUrl);
-        WebElement link = driver.findElement(By.xpath("//*[@id=\"j_id_jsp_416426823_1\"]/a"));
-        link.click();
+        //
+        //
+        //
+        //By.linkText("Show All Product Items")
+        WebElement productsLink = driver.findElement(By.xpath("//*[@id=\"j_id_jsp_416426823_1:linkProds\"]"));
+        
+        productsLink.click();
 
         try {
             TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException ex) {
             System.out.println(ex);
         }
-
+        //Siguiente pagina Web
         WebElement h1 = driver.findElement(By.tagName("h1"));
-
+        
         //Verificacion
         Assert.assertEquals(h1.getText(), "Listing Product Items");
+
+    }
+    
+    
+    @Test
+    public void createTest() {
+
+        String baseUrl = "http://localhost:9090/JsfDemoApp/faces/welcomeJSF.jsp";
+        
+        driver.get(baseUrl);
+        //
+        //By.linkText("Show All Product Items")
+        WebElement productsLink = driver.findElement(By.xpath("//*[@id=\"j_id_jsp_416426823_1:linkProds\"]"));
+        
+        productsLink.click();
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+        //Siguiente pagina Web
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        
+        //Link 'New Product'
+        WebElement linkNew = driver.findElement(By.xpath("//*[@id=\"j_id_jsp_2101894059_2\"]/a[1]"));
+        linkNew.click();
+        
+        String productIdValue = "1";
+        //Llenar Form
+        WebElement productId = driver.findElement(By.id("j_id_jsp_1869794631_2:productId"));
+        productId.sendKeys(productIdValue);
+        
+        WebElement purchaseCost = driver.findElement(By.id("j_id_jsp_1869794631_2:purchaseCost"));
+        purchaseCost.sendKeys("100");
+        
+        WebElement createButton = driver.findElement(By.xpath("//*[@id=\"j_id_jsp_1869794631_2\"]/a[1]"));
+        createButton.click();
+        
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+        
+        //En la pagina del listado de productos
+        WebElement nuevoProductId = driver.findElement(By.id("j_id_jsp_2101894059_2:j_id_jsp_2101894059_9:0:productIdColumn"));
+
+        //Verificacion
+        Assert.assertEquals(nuevoProductId.getText(), productIdValue);
 
     }
 
